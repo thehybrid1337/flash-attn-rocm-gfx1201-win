4470730c6a4260d484c481234a191dfe9100b151
thehybrid1337 <thehybrid1337@users.noreply.github.com>
2026-08-27 23:38:18 +0200

    Remove SageAttention from flash-attn repo (separate repo)

--- build/build-sageattention.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" >nul
-set PATH=%LOCALAPPDATA%\Programs\Python\Python312;%LOCALAPPDATA%\Programs\Python\Python312\Scripts;%PATH%
-set MAX_JOBS=16
-cd /d C:\sagebuild\SageAttention
-python setup.py bdist_wheel
